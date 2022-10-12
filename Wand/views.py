c254e888ed8d87742d24f6109d566ef7e6b34f2e
tienviet10 <viettran101294@gmail.com>
2022-10-12 16:29:59 -0400

    testing

--- Wand/views.py
+++ Wand/views.py
@@ -1,7 +1,10 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 from Wand.models import Shafts, Tips
 from Wand.serializers import ShaftsSerializer, TipsSerializer
@@ -9,6 +12,7 @@ from Wand.serializers import ShaftsSerializer, TipsSerializer
 # Create your views here.
 
 @csrf_exempt
+@api_view(['POST', 'GET', "PUT", "DELETE"])
 def tipsApi(request,id=0):
     if request.method=="GET":
         tips = Tips.objects.all()
@@ -19,7 +23,7 @@ def tipsApi(request,id=0):
         tips_serializer = TipsSerializer(data=tip_data)
         if tips_serializer.is_valid():
             tips_serializer.save()
-            return JsonResponse("Added Successfully",safe=False)
+            return Response("Added Successfully", status=status.HTTP_201_CREATED)
         return JsonResponse("Failed to Add",safe=False)
     elif request.method=="PUT":
         tip_data = JSONParser().parse(request)
