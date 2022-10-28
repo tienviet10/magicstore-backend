83f0680c1a9c6f2a077260ecd73f10005d5498cf
tienviet10 <viettran101294@gmail.com>
2022-10-28 01:20:36 -0400

    dephne adjusted v6

--- magicstore/asgi.py
+++ magicstore/asgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -17,8 +16,9 @@ from django.core.asgi import get_asgi_application
 import magicstore.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magicstore.settings')
-django.setup()
+
 django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket':AuthMiddlewareStack(
